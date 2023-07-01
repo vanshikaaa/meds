1d7ed10c49571437275f610530c8481053cca191
vanshikaaa <vanshikabalaji04@gmail.com>
2023-07-01 11:28:16 -0700

    added notifications

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@ import './App.css';
 import { useState } from "react";
 import Poopy from "./components/Poopy";
 import Dad from "./components/Dad";
+import addNotification from 'react-push-notification';
 
 function App() {
   const [poopy, setPoopy] = useState("");
+
+  const notification = () => {
+    setTimeout( () => {
+      addNotification({
+        title: 'Meds',
+        subtitle: 'Meds?',
+        message: 'tell ur mom i said hi!!!!!!!!!!!',
+        theme: 'darkblue',
+        native: true
+      });
+    }, 10000);
+  }
 
   return (
     <div className="App">
@@ -13,6 +26,7 @@ function App() {
           <button class="home" onClick={() => {setPoopy(true)}}>I am Poopy</button>
           <button class="home" onClick={() => {setPoopy(false)}}>I am Rick Alan Feese born April 2nd 1975 age 48</button>
         </> : <><button class="back" onClick={() => {setPoopy("")}}>Back</button> {(poopy) ? <Poopy/> : <Dad/>}</>}
+        <button onClick={() => {notification()}}>Testing???</button>
       </header>
     </div>
   );
